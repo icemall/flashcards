64b00cc962a795178d3668477a059e6fb29a8eab
Marat Yamaliev <marat@sumocoders.be>
2019-10-16 17:42:18 +0300

    refactors active links

--- app/helpers/nav_helper.rb
+++ app/helpers/nav_helper.rb
@@ -2,4 +2,9 @@ module NavHelper
   def activatable_class(controller)
     'active' if controller == controller_name
   end
+
+  def active_link_to(anchor, path, options={})
+    options.merge!(class: 'active') if controller_name == options[:controller_name]
+    link_to anchor, path, options
+  end
 end
