2f70277a44b0fe4ba4b7c22bda63aef401fa8f41
Marat Yamaliev <marat@sumocoders.be>
2019-10-16 18:36:50 +0300

    rubcop errors cleanup

--- Gemfile
+++ Gemfile
@@ -15,8 +15,8 @@ gem 'bootsnap', '>= 1.1.0', require: false
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
-  gem 'rspec-rails'
   gem 'rails-controller-testing'
+  gem 'rspec-rails'
   gem 'shoulda-matchers'
 end
 
--- spec/controllers/cards_controller_spec.rb
+++ spec/controllers/cards_controller_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe CardsController, type: :controller do
-  describe "GET index" do
-    it "assigns @cards" do
+  describe 'GET index' do
+    it 'assigns @cards' do
       card = Card.create(original_text: 'cat', translated_text: 'кошка', review_date: Date.today)
       get :index
       expect(assigns(:cards)).to eq([card])
     end
 
-    it "renders the index template" do
+    it 'renders the index template' do
       get :index
       expect(response).to render_template("index")
     end
