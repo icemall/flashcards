6dfdcf6b99f67ce510a3f8c2992fc9c0022b038d
Marat Yamaliev <marat@sumocoders.be>
2019-10-17 13:16:32 +0300

    card validates texts are different

--- app/models/card.rb
+++ app/models/card.rb
@@ -2,4 +2,15 @@
 
 class Card < ApplicationRecord
   validates :original_text, :translated_text, :review_date, presence: true
+  validate :texts_are_different
+
+  private
+
+  def normalize(string)
+    string.strip.downcase
+  end
+
+  def texts_are_different
+    errors.add(:base, I18n.t('texts_should_be_different')) if normalize(original_text) == normalize(translated_text) && original_text.present?
+  end
 end
