00c9aed174c3f20008105a20c539e5772d4c2dc8
Marat Yamaliev <marat@sumocoders.be>
2019-10-18 13:02:29 +0300

    moves custom validations out of Card model

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,20 +1,6 @@
 # frozen_string_literal: true
 
 class Card < ApplicationRecord
+  validates_with CardSaveValidator
   validates :original_text, :translated_text, :review_date, presence: true
-  validate :texts_are_different
-
-  private
-
-  def normalize(string)
-    return unless string
-
-    string.strip.downcase
-  end
-
-  def texts_are_different
-    if normalize(original_text) == normalize(translated_text) && original_text.present?
-      errors.add(:base, I18n.t('texts_should_be_different'))
-    end
-  end
 end
---
+++ app/validators/card_save_validator.rb
@@ -0,0 +1,17 @@
+class CardSaveValidator < ActiveModel::Validator
+  def validate(record)
+    if texts_are_different(record)
+      record.errors.add(:base, I18n.t('texts_should_be_different'))
+    end
+  end
+
+  private
+  def normalize(string)
+    return unless string
+    string.strip.downcase
+  end
+
+  def texts_are_different(record)
+    normalize(record.original_text) == normalize(record.translated_text) && record.original_text.present?
+  end
+end
