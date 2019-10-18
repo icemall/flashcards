0cbe3cc24e0a9d8a6d8a757f7c777bcc563e89e4
Marat Yamaliev <marat@sumocoders.be>
2019-10-18 13:22:42 +0300

    code cleanup

--- app/services/create_card.rb
+++ app/services/create_card.rb
@@ -1,7 +1,5 @@
 class CreateCard
   def self.call(options)
-    card = Card.new(options)
-    card.review_date = Date.today + 3.days
-    card
+    card = Card.new(options).tap{ |c| c.review_date = Date.today + 3.days }
   end
 end
