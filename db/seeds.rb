5459469433cdafe985106316b5d04f66e618a6b2
Marat Yamaliev <marat@sumocoders.be>
2019-11-06 16:26:25 +0300

    seeds refactoring

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,7 @@
 # frozen_string_literal: true
 
-# parse DE->EN translations for card seeds
-require 'open-uri'
-TRANSLATIONS_URL = 'http://1000mostcommonwords.com/1000-most-common-german-words/'
-page = Nokogiri::HTML(open(TRANSLATIONS_URL))
-cards = []
-page.css('tr').each_with_index do |row, i|
-  next if i.zero? # skip header
-
-  card = {}
-  card[:original_text] = row.css('td')[1].text
-  card[:translated_text] = row.css('td')[2].text
-  card[:review_date] = Date.today
-  cards << card
-end
-cards.each do |card|
-  CreateCard.call(card).save
+require "#{Rails.root}/lib/api/card_parser"
+Api::CardParser.call.each do |card|
+  card = CreateCard.call(card)
+  card.save! if card.valid?
 end
