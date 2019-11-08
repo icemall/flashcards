333cb4ffce4d87a469c4ab591cc78bfcbfcd1f93
Marat Yamaliev <marat@sumocoders.be>
2019-11-08 13:11:59 +0300

    Card.to_test scope test

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -6,4 +6,7 @@ RSpec.describe Card, type: :model do
   it { should validate_presence_of(:original_text) }
   it { should validate_presence_of(:translated_text) }
   it { should validate_presence_of(:review_date) }
+  it "applies cards with review date eq to today or earlier to 'to_test' scope" do
+    expect(Card.to_test).to eq Card.where('review_date <= ?', Date.today)
+  end
 end
