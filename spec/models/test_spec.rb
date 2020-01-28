e7915e03fe3cab58315afb5f240106497aad2e65
Marat Yamaliev <marat@sumocoders.be>
2020-01-28 15:03:57 +0300

    wip

--- spec/models/test_spec.rb
+++ spec/models/test_spec.rb
@@ -1,29 +1,26 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
-# TODO fix this
+
 RSpec.describe Test do
-  before do
-    let!(:card) { create(:testable_card) }
-  end
-  context 'with no card_id provided' do
+  subject(:card) { create(:testable_card) }
+
+  context 'when no card_id provided' do
     let(:test) { Test.new }
     it 'picks testable cards' do
       expect(Card.to_test).to eq([Test.new.card])
     end
   end
-  context 'with card_id provided and correct results' do
+
+  context 'when card_id provided and results are correct' do
     let(:test) { Test.new(card_id: card.id, translated_text: card.translated_text) }
-    it 'picks card using card_id param' do
-      expect(test.card_id).to eq(card.id)
-    end
 
     it 'updates card review_date' do
-      expect(test.call).to change(card.review_date)
+      expect { test.call }.to(change { card.review_date })
     end
 
     it 'sets success' do
-      expect(test.call).to change(card.succes)
+      expect { test.call }.to(change { card.success })
     end
   end
 end
