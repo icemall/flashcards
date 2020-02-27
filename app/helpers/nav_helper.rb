af16ecdaaefb7d32ea83f9b0e4ab6be1375864f8
Marat Yamaliev <marat@sumocoders.be>
2020-02-27 18:33:38 +0300

    refactors active link helper

--- app/helpers/nav_helper.rb
+++ app/helpers/nav_helper.rb
@@ -2,9 +2,15 @@
 
 module NavHelper
   def active_link_to(anchor, path, options = {})
-    options[:class] = 'active' if controller_name == options[:controller_name] && action_name == options[:action_name]
+    options[:class] = 'active' if active_action?(options)
     options.delete(:controller_name)
     options.delete(:action_name)
     link_to anchor, path, options
+  end
+
+  private
+
+  def active_action?(options)
+    controller_name == options[:controller_name] && action_name == options[:action_name]
   end
 end
