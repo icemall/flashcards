abd3d277caaacf268bb96bae76335b48bfc3562f
Marat Yamaliev <marat@sumocoders.be>
2020-02-28 16:29:04 +0300

    decorates test

--- Gemfile
+++ Gemfile
@@ -46,6 +46,7 @@ gem 'tzinfo-data', platforms: %i[mingw mswin x64_mingw jruby]
 gem 'carrierwave', '~> 2.0'
 gem 'fog-aws'
 gem 'font-awesome-rails'
+gem 'draper'
 gem 'rails-i18n'
 gem 'simple_form'
 gem 'slim'
---
+++ app/decorators/test_decorator.rb
@@ -0,0 +1,11 @@
+class TestDecorator < Draper::Decorator
+  delegate_all
+
+  def picture
+    return unless card.picture
+
+    h.content_tag :div, class: 'row text-center' do
+      h.tag :img, src: card.picture.url
+    end
+  end
+end
